8eda39b0f7a356fd05cc17a9e3335c3cdcb3b33f
李月茹 <liyueru@treefinance.com.cn>
2018-12-27 12:36:45 +0800

    述职报告

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,71 +1,71 @@
 module.exports = {
-    title: `YRcoder`,
-    base: '/blogs/',
-    themeConfig: {
-        repo: 'https://github.com/yrcoder',
-        // 如果你的文档不在仓库的根部
-        docsDir: 'docs',
-        // 可选，默认为 master
-        docsBranch: 'master',
-        // 默认为 true，设置为 false 来禁用
-        editLinks: false,
-        head: [['link', { rel: 'icon', href: `/favicon.ico` }]],
-        nav: [
-            { text: '语法', link: '/grammar/htmlCss' },
-            { text: '教育', link: '/education/theory' },
-            { text: '记录', link: '/date/2018' },
-            { text: '随笔', link: '/essay/2018' },
-            { text: '其他', link: '/other/fingerling' },
-            { text: '英文小故事', link: '/english/2018-12' },
-        ],
-        sidebar: {
-            '/grammar/': [
-                {
-                    title: '基础语法',
-                    collapsable: false,
-                    children: ['htmlCss', 'bom', 'dom', 'js', 'es6', 'git'],
-                },
-                {
-                    title: '其他语法',
-                    collapsable: false,
-                    children: ['vue', 'react', 'springBoot', 'node', 'mobx', 'router'],
-                },
-            ],
-            '/education/': [
-                {
-                    title: '教育',
-                    collapsable: false,
-                    children: ['theory', 'exam', 'other'],
-                },
-            ],
-            '/other/': [
-                {
-                    title: '小东西',
-                    collapsable: false,
-                    children: ['fingerling', 'project'],
-                },
-            ],
-            '/date/': [
-                {
-                    title: '日记',
-                    collapsable: false,
-                    children: ['2018', '2019', '2020'],
-                },
-            ],
-            '/essay/': [
-                {
-                    title: '随笔',
-                    collapsable: false,
-                    children: ['2018'],
-                },
-            ],
-            '/english/': [
-                {
-                    title: '英文小故事',
-                    collapsable: false,
-                    children: ['2018-12'],
-                },
-            ],
-        },
-    },
+	title: `YRcoder`,
+	base: '/blogs/',
+	themeConfig: {
+		repo: 'https://github.com/yrcoder',
+		// 如果你的文档不在仓库的根部
+		docsDir: 'docs',
+		// 可选，默认为 master
+		docsBranch: 'master',
+		// 默认为 true，设置为 false 来禁用
+		editLinks: false,
+		head: [['link', { rel: 'icon', href: `/favicon.ico` }]],
+		nav: [
+			{ text: '语法', link: '/grammar/htmlCss' },
+			{ text: '教育', link: '/education/theory' },
+			{ text: '记录', link: '/date/2018' },
+			{ text: '随笔', link: '/essay/2018' },
+			{ text: '其他', link: '/other/fingerling' },
+			{ text: '英文小故事', link: '/english/2018-12' },
+		],
+		sidebar: {
+			'/grammar/': [
+				{
+					title: '基础语法',
+					collapsable: false,
+					children: ['htmlCss', 'bom', 'dom', 'js', 'es6', 'git', 'weixin'],
+				},
+				{
+					title: '其他语法',
+					collapsable: false,
+					children: ['vue', 'react', 'springBoot', 'node', 'mobx', 'router'],
+				},
+			],
+			'/education/': [
+				{
+					title: '教育',
+					collapsable: false,
+					children: ['theory', 'exam', 'other'],
+				},
+			],
+			'/other/': [
+				{
+					title: '小东西',
+					collapsable: false,
+					children: ['fingerling', 'project'],
+				},
+			],
+			'/date/': [
+				{
+					title: '日记',
+					collapsable: false,
+					children: ['2018', '2019', '2020'],
+				},
+			],
+			'/essay/': [
+				{
+					title: '随笔',
+					collapsable: false,
+					children: ['2018', '2018WorkReport'],
+				},
+			],
+			'/english/': [
+				{
+					title: '英文小故事',
+					collapsable: false,
+					children: ['2018-12'],
+				},
+			],
+		},
+	},
 }
